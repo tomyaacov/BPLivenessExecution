bee72128614e7eeced239f31d430d6e0ecf6a904
tomyaacov <tomyaacov1210@gmail.com>
2021-11-18 12:32:42 +0200

    adding puzzle

--- sokoban_maps.py
+++ sokoban_maps.py
@@ -138,9 +138,28 @@ maps = {
 "XXXXXXXX"
     ],
 }
-
-"""
-[
+maps2 = {
+    0 : [
+    "XXXXXXXX",
+    "X   XXXX",
+    "X   X  X",
+    "XX     X",
+    "XX XXXtX",
+    "X bXXXXX",
+    "Xa XXXXX",
+    "XXXXXXXX",
+],
+    1:[
+    "XXXXXXXX",
+    "X     aX",
+    "Xt  b  X",
+    "X  tb  X",
+    "XXX X  X",
+    "XXXXXXXX",
+    "XXXXXXXX",
+    "XXXXXXXX",
+],
+    2: [
         "XXXXXXXX",
         "X XXX aX",
         "X t X  X",
@@ -150,6 +169,9 @@ maps = {
         "XX     X",
         "XXXXXXXX",
     ]
+}
+
+"""
 
 XXXXXXXX
 XXX  XXX
---
+++ train_sokoban_2.py
@@ -0,0 +1,42 @@
+import os
+import sys
+import matplotlib.pyplot as plt
+from bp_env import BPEnv
+from sokoban import *
+from q_learning import *
+from sokoban_maps import maps2
+import time
+import sys
+map_key = int(sys.argv[1])
+map_value = maps2[map_key]
+for j in range(100):
+    start = time.time()
+    env = BPEnv()
+    env.set_bprogram_generator(init_bprogram)
+    
+    pygame_settings["display"] = False
+    map_settings["map"] = map_value
+    
+    Q, results, episodes, mean_reward = qlearning(environment=env,
+                                                  num_episodes=int(sys.argv[2]),
+                                                  episode_timeout=100,
+                                                  alpha=0.1,
+                                                  gamma=0.99,
+                                                  testing=True,
+                                                  seed=1,
+                                                  glie=glie_10)
+    end = time.time()
+    #plt.plot(episodes, mean_reward)
+    #plt.ylabel('mean reward')
+    #plt.xlabel('episode')
+    #plt.title(os.path.basename(sys.argv[0])[:-3])
+    #plt.savefig(os.path.basename(sys.argv[0])[:-3] + ".pdf")
+    event_runs = []
+    rewards_sum = 0
+    for i in range(100):
+        reward, event_run = run(env, Q, i, 100, True)
+        if event_run not in event_runs:
+            event_runs.append(event_run)
+        rewards_sum += reward
+        
+    print(j, rewards_sum / 100, end - start)
